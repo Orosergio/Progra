047d1b53361a9f48c2fc3e071d61410dc7d71356
alumno <alumno@2B1.labs.academico>
2019-02-19 11:23:29 -0600

    Ultimos cambios.2ASA

--- src/MatricesArrays.java
+++ src/MatricesArrays.java
@@ -37,39 +37,17 @@ public class MatricesArrays extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        TablaPrincipal = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
         TablaDepartamento = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jScrollPane3 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        TablaPrincipal = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        TablaPrincipal.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
-        TablaPrincipal.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null, null, null}
-            },
-            new String [] {
-                "Código", "Nombres", "Salario Base", "Bonificación", "Comisión", "Descuento Judicial", "IGSS", "ISR", "Saldo Liquido", "Departamento", "saldo sin ISR"
-            }
-        ));
-        jScrollPane1.setViewportView(TablaPrincipal);
-
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 40, 1050, 190));
 
         TablaDepartamento.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
         TablaDepartamento.setModel(new javax.swing.table.DefaultTableModel(
@@ -121,71 +99,98 @@ public class MatricesArrays extends javax.swing.JFrame {
 
         getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 190, 80));
 
+        TablaPrincipal.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
+        TablaPrincipal.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "Código", "Nombres", "Salario Base", "Bonificación", "Comisión", "Descuento Judicial", "IGSS", "ISR", "Saldo Liquido", "Departamento", "saldo sin ISR", "Paga IGSS?"
+            }
+        ));
+        jScrollPane1.setViewportView(TablaPrincipal);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 50, 1100, 190));
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
     //codigo    nombre  sueldo+ bonificaion+    comision+   igss-   descuento judicial-     ISR-       sueldo liquido
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-    
+     random();
+        asignacionLetras();
+        iggs();
+        ISRcalcular();
         for (int i=0;i<10;i++){
-            for(int j=2;j<6;j++){
-              int intrandom=(int)(Math.random()*10001)+1000; //GENERANDO NÚMEROS RANDOM
-                matriz[i][j]=String.valueOf(intrandom);   //LLENANDO MATRIZ CON NUMEROS RANDOM
-                
-            }
-            asignacionNombres();
-            matriz[i][11]=JOptionPane.showInputDialog(null,"Paga IGSS? ");
-            igss(matriz);
-            //CALCULO SIN ISR
-            matriz[i][10]=String.valueOf(Integer.parseInt(matriz[i][2])+Integer.parseInt(matriz[i][3])+Integer.parseInt(matriz[i][4])-Integer.parseInt(matriz[i][5])-Float.parseFloat(matriz[i][6]));
-            asignacionDepartamentos();
-            isr(matriz);
-            
+        
             //SALDO LIQUIDO
             matriz[i][8]=String.valueOf(formato.format(Integer.parseInt(matriz[i][2])+Integer.parseInt(matriz[i][3])+Integer.parseInt(matriz[i][4])-Integer.parseInt(matriz[i][5])-Float.parseFloat(matriz[i][6])-Float.parseFloat(matriz[i][7])));
-   
+     
         }
+       
+        
+      
        this.MostrarMatriz(matriz,vectorLetras,vectorSumadep);
     
+    
     }//GEN-LAST:event_jButton2ActionPerformed
-    public String iggs(String matriz){
-        for(int i=0;i<10;i++){
-        if("SI".equals(matriz[i][11])){
-         //CALCULO IGSS
-            matriz[i][6]=String.valueOf(formato.format((Integer.parseInt(matriz[i][2])*4.83)/100));
-        }else if("NO".equals(matriz[i][11])){
-            matriz[i][6]="0";
-           
-        }
-        return matriz;
+  
+public void random(){
+         for (int i=0;i<10;i++){
+            for(int j=2;j<6;j++){
+              int intrandom=(int)(Math.random()*10000)+1000; //GENERANDO NÚMEROS RANDOM
+                matriz[i][j]=String.valueOf(intrandom);   //LLENANDO MATRIZ CON NUMEROS RANDOM
+                
+            }}
     }
     
-    public void asignacionNombres(){
-    
-        //INGRESO DE NOMBRE
+    public void iggs(){
         for(int i=0;i<10;i++){
+               matriz[i][11]=JOptionPane.showInputDialog(null,"Paga IGSS?\n1.- SI\n2.- NO");
+            if("1".equals(matriz[i][11]) ){
+            matriz[i][6]=String.valueOf((Integer.parseInt(matriz[i][2])*4.83)/100);
+        
+            }else if("2".equals(matriz[i][11])){
+            matriz[i][6]="0";
+            }
+            //CALCULO IGSS
+        }
+    }
+    
+    public void ISRcalcular(){
+        for(int i=0;i<10;i++){
+             //CALCULO SIN ISR....s
+            matriz[i][10]=String.valueOf(formato.format(Integer.parseInt(matriz[i][2])+Integer.parseInt(matriz[i][3])+Integer.parseInt(matriz[i][4])-Integer.parseInt(matriz[i][5])-Float.parseFloat(matriz[i][6])));
             //ASIGNACION DE CODIGO
             matriz[i][0]=String.valueOf(i+1);
-               matriz[i][1]=JOptionPane.showInputDialog(null,"Nombre: ");
-        }
-         
-    }
-    public String isr(String matriz){
-    //CALCULO ISR
-            if(Float.parseFloat(matriz[i][2])>=5000){
-               matriz[i][7]=String.valueOf(formato.format((Float.parseFloat(matriz[i][2])*5)/100));
-            }else if(Float.parseFloat(matriz[i][2])<5000){
-                matriz[i][7]=String.valueOf((Float.parseFloat(matriz[i][2])*3)/100);
+            //CALCULO ISR
+            if(Float.parseFloat(matriz[i][2])<5000){
+               matriz[i][7]=String.valueOf((Float.parseFloat(matriz[i][2])*3)/100);
+            }else if(Float.parseFloat(matriz[i][2])>5000&&Float.parseFloat(matriz[i][2])<10000){
+                matriz[i][7]=String.valueOf((Float.parseFloat(matriz[i][2])*5)/100);
             }else if(Float.parseFloat(matriz[i][2])>10000){
-                matriz[i][7]=String.valueOf((Float.parseFloat(matriz[i][2])*0.07));
+                matriz[i][7]=String.valueOf((Float.parseFloat(matriz[i][2])*7)/100);
             }
-        return matriz;
+            
+        }
     }
-    public void asignacionDepartamentos(){
-       for(int i=0;i<10;i++){
-           int intRandomdep=(int)(Math.random()*5)+0;
+public void asignacionLetras(){
+    
+        for(int i=0;i<10;i++){
+             matriz[i][1]=JOptionPane.showInputDialog(null,"Nombre: ");
+            int intRandomdep=(int)(Math.random()*5)+0;
             //ASIGNACION DE DEPARTAMENTO
             matriz[i][9]=vectorLetras[intRandomdep];
-            if (matriz [i][9] == "A"){
+            
+            if (matriz [i][9] == "A"){ 
             vectorSumadep[0]+= Integer.parseInt(matriz[i][4]);    
             }
             if (matriz [i][9] == "B"){
@@ -201,11 +206,10 @@ public class MatricesArrays extends javax.swing.JFrame {
             vectorSumadep[4]+=Integer.parseInt(matriz[i][4]);    
             }
         }
-        
-    }
+}
     
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        for(int i=0;i<10;i++){
+       for(int i=0;i<10;i++){
             for(int j=0;j<12;j++){
                 matriz[i][j]="";
             }
@@ -253,22 +257,22 @@ public class MatricesArrays extends javax.swing.JFrame {
     
     
     
-    public void MostrarMatriz(String matrix[][],String vector[],int vecSuma[]){
+     public void MostrarMatriz(String matrix[][],String vector[],int vecSuma[]){
         DefaultTableModel model = (DefaultTableModel) TablaPrincipal.getModel(); //Obteniendo modelo de JTable
         for(int i=0;i<10;i++){
-            for(int j=0;j<11;j++){
-                TablaPrincipal.setValueAt(matriz[i][j], i, j); //Asignando valores de la matriz al JTable
+            for(int j=0;j<12;j++){
+                TablaPrincipal.setValueAt(matriz[i][j], i, j); //Asignando valores de la matriz al JTable...
             }
         }
         
-        DefaultTableModel dftVectormodel=(DefaultTableModel) TablaDepartamento.getModel(); //Obtener el model de JTable
+        DefaultTableModel dftVectormodel=(DefaultTableModel) TablaDepartamento.getModel(); //Obtener el model de JTable..
         for (int i=0;i<5;i++){
             for(int j=0;j<2;j++){
             TablaDepartamento.setValueAt(vecSuma[i],i,0);    
             }
          }
     }
-    
+               
                
            
     
