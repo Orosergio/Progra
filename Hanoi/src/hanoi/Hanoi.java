fc3727d303a18d21f6392e6e2a884c91ed3dfa1e
U. Mariano Gálvez <alumno@labs.academico>
2019-05-14 11:04:23 -0600

    Documentacion

--- Hanoi/src/hanoi/Hanoi.java
+++ Hanoi/src/hanoi/Hanoi.java
@@ -21,44 +21,45 @@ public class Hanoi {
     public static Pilas pPila3=new Pilas();
     public static int topc;
     public static void main(String[] args) {
-        int iCantidadDiscos;
+        int iCantidadDiscos;//Declaracion para los discos
+        //Obtiene el numero de discos que desea utilizar
         iCantidadDiscos=Integer.parseInt(JOptionPane.showInputDialog("Cuantos discos desea?"));
            for (int i = 1; i <= iCantidadDiscos; i++) {
-               pPila1.PUSH(i);
+               pPila1.PUSH(i);//Ingrese los discos a la pila
            }
-        System.out.println("Origen\t\t"+pPila1.Listar()+ "\nAuxiliar\t\t"+pPila2.Listar()+"\nDestino\t\t"+pPila3.Listar()+"\n");
-       PasosHanoi(Integer.parseInt(pPila1.TOP()),1,2,3);
+        System.out.println("Origen\t\t"+pPila1.Listar()+ "\nAuxiliar\t\t"+pPila2.Listar()+"\nDestino\t\t"+pPila3.Listar()+"\n");//Muestro las pilas
+       PasosHanoi(Integer.parseInt(pPila1.TOP()),1,2,3);//Envia los parametros al metodo
     }          
     public static void PasosHanoi(int Numero, int iOrigen, int iAuxiliar, int iDestino){
         if (Numero==1) {
           //origen a destino
          // System.out.println("mover disco de " + iOrigen + " a " + iDestino);
        
-         if(iOrigen==1){
-            topc=Integer.parseInt(pPila1.TOP()); 
-            pPila1.POP();
+         if(iOrigen==1){//Si se encuentra en la primera pila
+            topc=Integer.parseInt(pPila1.TOP()); //Se obtiene el valor que este en lo alto de la pila
+            pPila1.POP();//Se saca el ultimo valor
             }
             if(iOrigen==2){
-            topc=Integer.parseInt(pPila2.TOP()); 
-            pPila2.POP();
+            topc=Integer.parseInt(pPila2.TOP()); //Se obtiene el valor que este en lo alto de la pila
+            pPila2.POP();//Se saca el ultimo valor
             
             } 
             
             if(iOrigen==3){
-            topc=Integer.parseInt(pPila3.TOP()); 
-            pPila3.POP();
+            topc=Integer.parseInt(pPila3.TOP()); //Se obtiene el valor que este en lo alto de la pila
+            pPila3.POP();//Se saca el ultimo valor
             }
             
             if(iDestino==1){
-                pPila1.PUSH(topc);
+                pPila1.PUSH(topc);//Se ingresa a la pila 1 el valor obtenido de la cima
             }
             if(iDestino==2){
-                pPila2.PUSH(topc);
+                pPila2.PUSH(topc);//Se ingresa a la pila 2 el valor obtenido de la cima
             }
             if(iDestino==3){
-                pPila3.PUSH(topc);
+                pPila3.PUSH(topc);//Se ingresa a la pila 3 el valor obtenido de la cima
             }
-           System.out.println("Origen: "+pPila1.Listar()+"\nAuxiliar: "+pPila2.Listar()+"\nDestino: "+pPila3.Listar());
+           System.out.println("Origen: "+pPila1.Listar()+"\nAuxiliar: "+pPila2.Listar()+"\nDestino: "+pPila3.Listar());//Muestro todos los valores
            
             System.out.println("");
         }else{
@@ -66,36 +67,36 @@ public class Hanoi {
            
             System.out.println("Mover disco de "+iOrigen+" a " +iDestino);
             if(iOrigen==1){
-            topc=Integer.parseInt(pPila1.TOP()); 
-            pPila1.POP();
+            topc=Integer.parseInt(pPila1.TOP()); //Se obtiene el valor que este en lo alto de la pila
+            pPila1.POP();//Se saca el ultimo valor
             }
             if(iOrigen==2){
-            topc=Integer.parseInt(pPila2.TOP()); 
-            pPila2.POP();
+            topc=Integer.parseInt(pPila2.TOP()); //Se obtiene el valor que este en lo alto de la pila
+            pPila2.POP();//Se saca el ultimo valor
             
             } 
             
             if(iOrigen==3){
-            topc=Integer.parseInt(pPila3.TOP()); 
-            pPila3.POP();
+            topc=Integer.parseInt(pPila3.TOP()); //Se obtiene el valor que este en lo alto de la pila
+            pPila3.POP();//Se saca el ultimo valor
             }
             
             if(iDestino==1){
-                pPila1.PUSH(topc);
+                pPila1.PUSH(topc);//Se ingresa a la pila 1 el valor obtenido de la cima
             }
             if(iDestino==2){
-                pPila2.PUSH(topc);
+                pPila2.PUSH(topc);//Se ingresa a la pila 2 el valor obtenido de la cima
             }
             if(iDestino==3){
-                pPila3.PUSH(topc);
+                pPila3.PUSH(topc);//Se ingresa a la pila 3 el valor obtenido de la cima
             }
             System.out.println("\nOrigen: "+pPila1.Listar()+"\nAuxiliar: "+pPila2.Listar()+"\nDestino: "+pPila3.Listar());    
              try{
-            Thread.sleep(500);
+            Thread.sleep(500);//Es un delay para poder mostrar como se hacen los cambios
         }catch(InterruptedException e){
             
         }
-            PasosHanoi(Numero-1,iAuxiliar,iOrigen,iDestino);
+            PasosHanoi(Numero-1,iAuxiliar,iOrigen,iDestino);//Se utiliza metodo de recursividad
         
         }
         
